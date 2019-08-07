d1c5cbfd8798356de30dc6ee5ea1457b60e04fae
Lynn Eitelman <lheitelman@gmail.com>
2019-08-07 15:11:03 -0400

    Add files via upload

---
+++ python HW.py
@@ -0,0 +1,24 @@
+# Modules
+import os
+import csv
+
+counter = 0
+nettotal = 0
+
+path = "budget_data.csv"
+
+with open(path, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+        
+    # let's call the header:
+    csv_header = next(csvfile)
+    print(csv_header)
+
+    for row in csvreader:
+        print(row)
+        nettotal = nettotal + int(row[1])
+        counter = counter + 1
+
+
+print(nettotal)
+print(counter)
